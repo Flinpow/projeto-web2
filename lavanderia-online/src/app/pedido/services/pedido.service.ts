bfce316ab59a2dd4fe825e0d94857e26dd5d9e23
Flinpow <thalesgc2011@hotmail.com>
2023-09-01 15:31:01 -0300

    Criando modulo de pedido E criando botão de "Recolher" nos componentes de listagem

--- lavanderia-online/src/app/pedido/services/pedido.service.ts
+++ lavanderia-online/src/app/pedido/services/pedido.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 // precisa vincular a criação dos models
 //import { Pedido } from '../pedido.service';
 
-//estratégia de buscar no localStorage como visto no projeto CRUD do professor
+//  INUTILIZAVEL ATENÇÃO: UTILIZAR SERVICE DECLARADA DENTRO DO MODULO DE PEDIDOS
 const LS_CHAVE: string = "pedidos";
 @Injectable({
   providedIn: 'root'
@@ -23,49 +23,6 @@ export class PedidoService {
     this.pedidos.push(novoPedido);
     this.numeroPedidoAtual++;
     return novoPedido;
-  }
-// Implementação de CRUD conforme mostrado pelo professor no projeto de CRUD.
-  listarTodos(): Pedido[] {
-    const pedidos = localStorage[LS_CHAVE];
-    return pedidos ? JSON.parse(pedidos) : [];
-  }
-
-  inserir(pedido: Pedido) {
-    const pedidos = this.listarTodos();
-    pedido.numero = new Date().getTime().toString();
-    pedidos.push(pedido);
-    localStorage[LS_CHAVE] = JSON.stringify(pedidos);
-  }
-
-  buscarPorId(numero: string) : Pedido | undefined {
-    // obtém a lista completa
-    const pedidos : Pedido[] = this.listarTodos();
-    // Efetua a busca
-    // find() retorna o primeiro elemento que satisfaz a condição
-    return pedidos.find(pedido => pedido.numero === numero);
-  }
-
-  atualiza(pedido: Pedido) {
-    //Obtém a lista completa
-    const pedidos: Pedido [] = this.listarTodos();
-    // varre a lista de pessoas 
-    // quando encontra a pessoa com o mesmo id, altera a lista
-    pedidos.forEach((obj, index, objs) => {
-      if(pedido.numero === obj.numero) {
-        objs[index] = pedido;
-      }
-    })
-    // armazena a nova lista no local storage
-      localStorage[LS_CHAVE] = JSON.stringify(pedido);
-  }
-
-  remover(numero: string) {
-    let pedidos: Pedido[] = this.listarTodos();
-    // filter() vai retornar todos da lista que forem 
-    //diferentes do id passado para exclusão
-    pedidos = pedidos.filter(pessoa => pessoa.numero !== numero);
-
-    localStorage[LS_CHAVE] = JSON.stringify(pedidos);
   }
 }
 
---
+++ lavanderia-online/src/app/pedidos/service/pedido.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { Orcamento } from 'src/app/shared/models/orcamento.model';
+import { Pedido } from 'src/app/shared/models/pedido.model';
+
+//estratégia de buscar no localStorage como visto no projeto CRUD do professor
+const LS_CHAVE: string = "pedidos";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidoService {
+  private pedidos: Pedido[] = [];
+  private numeroPedidoAtual = 1;
+
+  constructor() { }
+  //TODO - Refatorar método a seguir para atender a noa estrutura de Model Pedido
+//  aprovarPedido(orcamento: Orcamento): Pedido {
+//    const novoPedido: Pedido = {
+//      numero: `PEDIDO${this.numeroPedidoAtual}`,
+//      orcamento,
+//      estado: 'EM ABERTO'
+//    };
+//    this.pedidos.push(novoPedido);
+//    this.numeroPedidoAtual++;
+//    return novoPedido;
+//  }
+
+// Implementação de CRUD conforme mostrado pelo professor no projeto de CRUD.
+  listarTodos(): Pedido[] {
+    const pedidos = localStorage[LS_CHAVE];
+    return pedidos ? JSON.parse(pedidos) : [];
+  }
+
+  inserir(pedido: Pedido) {
+    const pedidos = this.listarTodos();
+    pedido.id = new Date().getTime();
+    pedidos.push(pedido);
+    localStorage[LS_CHAVE] = JSON.stringify(pedidos);
+  }
+
+  buscarPorId(id: number) : Pedido | undefined {
+    // obtém a lista completa
+    const pedidos : Pedido[] = this.listarTodos();
+    // Efetua a busca
+    // find() retorna o primeiro elemento que satisfaz a condição
+    return pedidos.find(pedido => pedido.id === id);
+  }
+
+  atualiza(pedido: Pedido) {
+    //Obtém a lista completa
+    const pedidos: Pedido [] = this.listarTodos();
+    // varre a lista de pessoas 
+    // quando encontra a pessoa com o mesmo id, altera a lista
+    pedidos.forEach((obj, index, objs) => {
+      if(pedido.id === obj.id) {
+        objs[index] = pedido;
+      }
+    })
+    // armazena a nova lista no local storage
+      localStorage[LS_CHAVE] = JSON.stringify(pedido);
+  }
+
+  remover(id: number) {
+    let pedidos: Pedido[] = this.listarTodos();
+    // filter() vai retornar todos da lista que forem 
+    //diferentes do id passado para exclusão
+    pedidos = pedidos.filter(pedido => pedido.id !== id);
+
+    localStorage[LS_CHAVE] = JSON.stringify(pedidos);
+  }
+
+  recolherPedido(id:number) {
+    let pedidos: Pedido[] = this.listarTodos();
+    //busca o pedido a ser alterado para 'RECOLHIDO'
+    let newPedido: Pedido | undefined= this.buscarPorId(id);
+    if(newPedido != undefined){
+      newPedido.estado = "RECOLHIDO";
+      this.atualiza(newPedido);
+    } else
+    throw new Error("Pessoa não encontrada: id = " + id);
+  }
+}
